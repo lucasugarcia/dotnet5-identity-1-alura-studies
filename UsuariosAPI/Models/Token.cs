1551a01f33b9be8c9396291cf1482aa6f21422f4
Lucas Garcia <lucasugarcia@gmail.com>
2022-09-03 19:14:19 -0300

    Gerando token JWT

---
+++ UsuariosAPI/Models/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuariosAPI.Models
+{
+    public class Token
+    {
+        public string Value { get; set; }
+
+        public Token(string value)
+        {
+            Value = value;
+        }
+    }
+}
